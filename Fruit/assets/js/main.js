0e0297505467b2d189248616bdb72369a1b3703b
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-09-02 12:26:04 +0800

    add: toggle menu script

--- Fruit/assets/js/main.js
+++ Fruit/assets/js/main.js
@@ -1,19 +1,26 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
+if (navToggle && navMenu && navClose) {
+    navToggle.addEventListener("click", () =>
+        navMenu.classList.add("show-menu")
+    );
+
+    navClose.addEventListener("click", () =>
+        navMenu.classList.remove("show-menu")
+    );
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== SHADOW HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
